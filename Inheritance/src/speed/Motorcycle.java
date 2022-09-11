2266a2419c3e6fb4e60cf8c699eafc4c81c99108
PreslavZlatinov <preslav.zlatinov@gmail.com>
2022-09-11 14:21:52 +0300

    Adding Inheritance Lesson

---
+++ Inheritance/src/speed/Motorcycle.java
@@ -0,0 +1,9 @@
+package speed;
+
+public class Motorcycle extends Vehicle{
+
+    public Motorcycle(double fuel, int horsePower) {
+        super(fuel, horsePower);
+        super.setFuelConsumption(fuelConsumption);
+    }
+}
